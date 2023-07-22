e0f9e77f838c5827599a836984ea5b029c68fa5f
Anjani <jhaanjani57@gmail.com>
2023-07-22 17:46:43 +0530

    added handle on change and handluUp Click

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -1,14 +1,34 @@
 import React from "react";
+import { useState } from "react";
 
 export default function TextForm(props) {
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleUpClick = () => {
+    console.log("upper case");
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const [text, setText] = useState("Enter text here");
   return (
     <div>
       <h1>{props.heading}</h1>
 
       <div className="mb-3">
-        <textarea className="form-control" id="myBox" rows="8"></textarea>
+        <textarea
+          className="form-control"
+          onChange={handleOnChange}
+          value={text}
+          id="myBox"
+          rows="8"
+        ></textarea>
       </div>
-      <button className="btn btn-primary">Convert to upper case</button>
+      <button className="btn btn-primary" onClick={handleUpClick}>
+        Convert to upper case
+      </button>
     </div>
   );
 }
