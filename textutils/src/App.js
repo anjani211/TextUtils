cc42a8c75021dbc0b26e6a6f78c21f0e56576d05
Anjani <jhaanjani57@gmail.com>
2023-07-24 01:32:13 +0530

    added react router

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import "./App.css";
-// import About from "./components/About";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light"); //Whether dark mode enabled or not
@@ -32,21 +33,30 @@ function App() {
   };
   return (
     <>
-      <Navbar
-        title="TextUtils"
-        aboutText="About"
-        mode={mode}
-        toggleMode={toggleMode}
-      />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <TextForm
-          showAlert={showAlert}
-          heading="Enter the text to analyse"
+      <Router>
+        <Navbar
+          title="TextUtils"
+          aboutText="About"
           mode={mode}
+          toggleMode={toggleMode}
         />
-        {/* <About /> */}
-      </div>
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+
+            <Route path="/">
+              <TextForm
+                showAlert={showAlert}
+                heading="Enter the text to analyse"
+                mode={mode}
+              />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </>
   );
 }
--- textutils/src/components/About.js
+++ textutils/src/components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function About() {
   return (
-    <div className="container" style={myStyle}>
+    <div className="container">
       <h1 className="my-4"> About Us</h1>
       <div className="accordion" id="accordionExample">
         <div className="accordion-item">
